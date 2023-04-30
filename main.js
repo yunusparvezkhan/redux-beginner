ab509c34e198a62dde1d686fa19895d50bd99d37
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-30 15:51:08 +0530

    v1 Delta 1.4
    completed the reducer function of claimsHistory department

--- main.js
+++ main.js
@@ -34,10 +34,15 @@ const createClaim = (name, claimAmount) => {
 
 // Reducer functions. Departments in our project
 
-const claimsHistory = (oldListOfClaims, action) => {
-    if (action.type === 'CREATE_POLICY') {
+const claimsHistory = (oldListOfClaims = [], action) => {
+    if (action.type === 'CLAIM_POLICY') {
         // add claim on the claims list
+        return [...oldListOfClaims, action.payload]; // This adds action.payload to a new array following the oldListofClaims.
     } else {
         //we dont care about this action
-    }
+        return oldListOfClaims; // This will return back oldListofClaims.
+    };
 }
+
+
+
